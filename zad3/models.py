17ce8a008fdf10ed3a266ca5afbac6d7340d9188
michalf1703 <126731293+michalf1703@users.noreply.github.com>
2023-12-31 09:03:37 +0100

    deleting points

--- zad3/models.py
+++ zad3/models.py
@@ -7,3 +7,12 @@ class DataPoint(db.Model):
     feature1 = db.Column(db.Float, nullable=False)
     feature2 = db.Column(db.Float, nullable=False)
     category = db.Column(db.Integer, nullable=False)
+
+    @staticmethod
+    def delete_record(record_id):
+        record = DataPoint.query.get(record_id)
+        if record:
+            db.session.delete(record)
+            db.session.commit()
+            return True
+        return False
--- zad3/views.py
+++ zad3/views.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from models import db, DataPoint
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SECRET_KEY'] = '3CqwrAzIPv'
 
 db.init_app(app)
 
@@ -29,5 +30,13 @@ def add():
 
     return render_template('add.html')
 
+@app.route('/delete/<int:record_id>', methods=['POST'])
+def delete(record_id):
+    if DataPoint.delete_record(record_id):
+        flash('Record deleted successfully', 'success')
+    else:
+        flash('Record not found', 'error')
+
+    return redirect(url_for('index'))
 if __name__ == '__main__':
     app.run(debug=True)
